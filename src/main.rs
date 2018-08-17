f7f1ab830ab6bb67d0e0d3f255ea94841f7e2b61
Thom Chiovoloni <chiovolonit@gmail.com>
2018-08-17 10:52:27 -0700

    anonymize all cols of all tables

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,12 @@ extern crate failure;
 #[macro_use]
 extern crate log;
 extern crate rand;
-extern crate tempfile;
-extern crate url;
 extern crate clap;
 extern crate env_logger;
-
 
 use rand::prelude::*;
 use std::{process, fs, path::{Path, PathBuf}};
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-
 
 use rusqlite::{Connection, OpenFlags};
 
@@ -67,7 +62,7 @@ fn get_profiles() -> Result<Vec<Profile>> {
         }
     }
     debug!("Using profile path: {:?}", path);
-    let mut res = fs::read_dir(path)?
+    let res = fs::read_dir(path)?
     .map(|entry_result| {
         let entry = entry_result?;
         trace!("Considering path {:?}", entry.path());
@@ -136,6 +131,27 @@ impl StringAnonymizer {
 
 }
 
+#[derive(Debug, Clone)]
+struct TableInfo {
+    name: String,
+    cols: Vec<String>
+}
+
+impl TableInfo {
+    fn for_table(name: String, conn: &Connection) -> Result<TableInfo> {
+        let stmt = conn.prepare(&format!("SELECT * FROM {}", name))?;
+        let cols = stmt.column_names().into_iter().map(|x| x.to_owned()).collect();
+        Ok(TableInfo { name, cols })
+    }
+    fn make_update(&self, updater_fn: &str) -> String {
+        let sets = self.cols.iter()
+            .map(|col| format!("{} = {}({})", col, updater_fn, col))
+            .collect::<Vec<_>>()
+            .join(",\n    ");
+        format!("UPDATE {}\nSET {}", self.name, sets)
+    }
+}
+
 fn main() -> Result<()> {
     let matches = clap::App::new("anonymize-places")
         .arg(clap::Arg::with_name("OUTPUT")
@@ -194,225 +210,43 @@ fn main() -> Result<()> {
     }
 
     fs::copy(&profile.places_db, &output_path)?;
-
-    // Copy `places.sqlite` into a temp file because if firefox is currently
-    // open, we'll have issues reading from it.
-    debug!("Copying places.sqlite to a temp directory for reading");
-
-    // let tmp_dir = tempfile::tempdir()?;
-    // let read_copy_path = tmp_dir.path().join("places.sqlite");
-    // fs::copy(&read_copy_path, &profile.places_db)?;
-    
-
-    // let places = Connection::open_with_flags(&read_copy_path, OpenFlags::SQLITE_OPEN_READ_ONLY)?;
-
     let anon_places = Connection::open_with_flags(&output_path,
-        //OpenFlags::SQLITE_OPEN_CREATE |
         OpenFlags::SQLITE_OPEN_READ_WRITE)?;
 
-    let anonymizer = Arc::new(Mutex::new(StringAnonymizer::default()));
     {
-        let anonymizer = anonymizer.clone();
+        let mut anonymizer = StringAnonymizer::default();
         anon_places.create_scalar_function("anonymize", 1, true, move |ctx| {
-            let mut a = anonymizer.lock().unwrap();
-            let arg = ctx.get::<String>(0)?;
-            let res = a.anonymize(&arg);
-            Ok(res)
+            let arg = ctx.get::<rusqlite::types::Value>(0)?;
+            Ok(match arg {
+                rusqlite::types::Value::Text(s) =>
+                    rusqlite::types::Value::Text(anonymizer.anonymize(&s)),
+                not_text => not_text
+            })
         })?;
     }
 
-    anon_places.execute_batch("
-        BEGIN;
-            -- TODO: anonymize should do the right thing for NULL (it's just annoying)
-            UPDATE moz_origins
-            SET prefix = anonymize(IFNULL(prefix, '')),
-                host = anonymize(IFNULL(host, ''));
-
-            UPDATE moz_inputhistory
-            SET input = anonymize(IFNULL(input, ''));
-
-            UPDATE moz_places
-            SET url = anonymize(url),
-                title = anonymize(IFNULL(title, '')),
-                rev_host = anonymize(IFNULL(rev_host, '')),
-                description = anonymize(IFNULL(description, '')),
-                preview_image_url = anonymize(IFNULL(preview_image_url, '')),
-                url_hash = 0;
-
-            -- We don't have HASH and I don't feel like porting
-            -- https://searchfox.org/mozilla-central/source/toolkit/components/places/Helpers.cpp#308
-            -- to Rust.
-
-            -- UPDATE moz_places
-            -- SET url_hash = HASH(url)
-
-            UPDATE moz_bookmarks
-            SET title  = anonymize(IFNULL(title, '')),
-                folder_type = anonymize(IFNULL(folder_type, ''));
-
-            DELETE FROM moz_hosts;
-            DELETE FROM moz_anno_attributes;
-            DELETE FROM moz_annos;
-            DELETE FROM moz_items_annos;
-            DELETE FROM moz_bookmarks_deleted;
-            DELETE FROM moz_keywords;
-        COMMIT;
-        VACUUM;
-    ")?;
-
-
-
-
-
-// table|moz_inputhistory|moz_inputhistory|4|CREATE TABLE moz_inputhistory (  place_id INTEGER NOT NULL, input LONGVARCHAR NOT NULL, use_count INTEGER, PRIMARY KEY (place_id, input))
-
-// table|moz_hosts|moz_hosts|6|CREATE TABLE moz_hosts (  id INTEGER PRIMARY KEY, host TEXT NOT NULL UNIQUE, frecency INTEGER, typed INTEGER NOT NULL DEFAULT 0, prefix TEXT)
-// table|moz_bookmarks|moz_bookmarks|8|CREATE TABLE moz_bookmarks (
-//     id INTEGER PRIMARY KEY,
-//     type INTEGER,
-//     fk INTEGER DEFAULT NULL, parent INTEGER, position INTEGER,
-//     title LONGVARCHAR,
-//     keyword_id INTEGER,
-//     folder_type TEXT, dateAdded INTEGER,
-//     lastModified INTEGER,
-//     guid TEXT,
-//     syncStatus INTEGER NOT NULL DEFAULT 0,
-//     syncChangeCounter INTEGER NOT NULL DEFAULT 1
-// )
-// table|moz_bookmarks_deleted|moz_bookmarks_deleted|9|CREATE TABLE moz_bookmarks_deleted (  guid TEXT PRIMARY KEY, dateRemoved INTEGER NOT NULL DEFAULT 0)
-// table|moz_keywords|moz_keywords|11|CREATE TABLE moz_keywords (  id INTEGER PRIMARY KEY AUTOINCREMENT, keyword TEXT UNIQUE, place_id INTEGER, post_data TEXT)
-
-// table|moz_anno_attributes|moz_anno_attributes|14|CREATE TABLE moz_anno_attributes (  id INTEGER PRIMARY KEY, name VARCHAR(32) UNIQUE NOT NULL)
-// table|moz_annos|moz_annos|16|CREATE TABLE moz_annos (  id INTEGER PRIMARY KEY, place_id INTEGER NOT NULL, anno_attribute_id INTEGER, content LONGVARCHAR, flags INTEGER DEFAULT 0, expiration INTEGER DEFAULT 0, type INTEGER DEFAULT 0, dateAdded INTEGER DEFAULT 0, lastModified INTEGER DEFAULT 0)
-// table|moz_items_annos|moz_items_annos|17|CREATE TABLE moz_items_annos (  id INTEGER PRIMARY KEY, item_id INTEGER NOT NULL, anno_attribute_id INTEGER, content LONGVARCHAR, flags INTEGER DEFAULT 0, expiration INTEGER DEFAULT 0, type INTEGER DEFAULT 0, dateAdded INTEGER DEFAULT 0, lastModified INTEGER DEFAULT 0)
-
-// table|moz_meta|moz_meta|19|CREATE TABLE moz_meta (key TEXT PRIMARY KEY, value NOT NULL) WITHOUT ROWID
-
-
-
-    // {
-    //     debug!("Copying schema");
-    //     let mut stmt = places.prepare("SELECT sql FROM sqlite_master")?;
-    //     let mut rows = stmt.query(&[])?;
-
-    //     while let Some(row_or_error) = rows.next() {
-    //         let row = row_or_error?;
-    //         let s: String = row.get("sql");
-    //         anon_places.execute(&s, &[])?;
-    //     }
-    // }
-    /*
-    {
-        debug!("Anonymizing moz_origins");
-        let tx = anon_places.transaction()?;
-        let mut stmt = places.prepare("SELECT * FROM moz_origins")?;
+    let schema = {
+        let mut stmt = anon_places.prepare("
+            SELECT name FROM sqlite_master
+            WHERE type = 'table'
+              AND name NOT IN ('sqlite_sequence', 'sqlite_stat1')
+        ")?;
         let mut rows = stmt.query(&[])?;
-
+        let mut tables = vec![];
         while let Some(row_or_error) = rows.next() {
-            let row = row_or_error?;
-            let prefix = anonymizer.anonymize(&row.get::<_, String>("prefix"));
-            let host = anonymizer.anonymize(&row.get::<_, String>("host"));
-
-
-            tx.execute("
-                INSERT INTO moz_origins(id, prefix, host, frecency) VALUES (?, ?, ?, ?)
-            ", &[
-                &row.get::<_, i64>("id"),
-                &prefix,
-                &host,
-                &row.get::<_, i64>("frecency"),
-            ])?;
+            tables.push(TableInfo::for_table(row_or_error?.get("name"), &anon_places)?);
         }
-        tx.commit()?;
+        tables
+    };
+
+    for info in schema {
+        let sql = info.make_update("anonymize");
+        debug!("Executing sql:\n{}", sql);
+        anon_places.execute(&sql, &[])?;
     }
-    /*
-    {
-        debug!("Copying moz_origins");
-        let tx = anon_places.transaction()?;
-        let mut stmt = places.prepare("SELECT * FROM moz_origins")?;
-        let mut rows = stmt.query(&[])?;
+    debug!("Clearing places url_hash");
+    anon_places.execute("UPDATE moz_places SET url_hash = 0", &[])?;
+    println!("Done!");
 
-        while let Some(row_or_error) = rows.next() {
-            let row = row_or_error?;
-            let prefix = anonymizer.anonymize(&row.get::<_, String>("prefix"));
-            let host = anonymizer.anonymize(&row.get::<_, String>("host"));
-
-            tx.execute("
-                INSERT INTO moz_origins(id, prefix, host, frecency) VALUES (?, ?, ?, ?)
-            ", &[
-                &row.get::<_, i64>("id"),
-                &prefix,
-                &host,
-                &row.get::<_, i64>("frecency"),
-            ])?;
-        }
-        tx.commit()?;
-    }
-    {
-        debug!("Copying moz_places");
-        let tx = anon_places.transaction()?;
-        let mut stmt = places.prepare("SELECT * FROM moz_origins")?;
-        let mut rows = stmt.query(&[])?;
-
-        while let Some(row_or_error) = rows.next() {
-            let row = row_or_error?;
-            let id = row.get::<_, i64>("id");
-            let url = anonymizer.anonymize(&row.get::<_, String>("url"));
-            let title = anonymizer.anonymize(&row.get::<_, String>("title"));
-            let rev_host = anonymizer.anonymize(&row.get::<_, String>("rev_host"));
-            let visit_count = row.get::<_, i64>("visit_count");
-            let hidden = row.get::<_, i64>("hidden");
-            let typed = row.get::<_, i64>("typed");
-            let frecency = row.get::<_, i64>("frecency");
-            let last_visit_date = row.get::<_, i64>("last_visit_date");
-            let guid = row.get::<_, i64>("guid");
-            let foreign_count = row.get::<_, i64>("foreign_count");
-            // let url_hash = row.get::<_, i64>("url_hash")
-            let description = anonymizer.anonymize(&row.get::<_, String>("description"));
-            let preview_image_url = anonymizer.anonymize(&row.get::<_, String>("preview_image_url"));
-            let origin_id = row.get::<_, i64>("origin_id");
-            tx.execute("
-                INSERT INTO moz_origins(
-                    id, url, title, rev_host, visit_count, hidden, typed, frecency,
-                    last_visit_date, guid, foreign_count, description
-                ) VALUES (?, ?, ?, ?)
-            ", &[
-                &id, 
-            ])?;
-        }
-        tx.commit()?;
-        anon_places.execute("UPDATE moz_places SET url_hash = HASH(url)")?;
-    }*/
-    */
-
-/*
-
-
-table|moz_places|moz_places|2|CREATE TABLE moz_places (
-)
-
-table|moz_historyvisits|moz_historyvisits|3|CREATE TABLE moz_historyvisits (  id INTEGER PRIMARY KEY, from_visit INTEGER, place_id INTEGER, visit_date INTEGER, visit_type INTEGER, session INTEGER)
-table|moz_inputhistory|moz_inputhistory|4|CREATE TABLE moz_inputhistory (  place_id INTEGER NOT NULL, input LONGVARCHAR NOT NULL, use_count INTEGER, PRIMARY KEY (place_id, input))
-table|moz_hosts|moz_hosts|6|CREATE TABLE moz_hosts (  id INTEGER PRIMARY KEY, host TEXT NOT NULL UNIQUE, frecency INTEGER, typed INTEGER NOT NULL DEFAULT 0, prefix TEXT)
-table|moz_bookmarks|moz_bookmarks|8|CREATE TABLE moz_bookmarks (  id INTEGER PRIMARY KEY, type INTEGER, fk INTEGER DEFAULT NULL, parent INTEGER, position INTEGER, title LONGVARCHAR, keyword_id INTEGER, folder_type TEXT, dateAdded INTEGER, lastModified INTEGER, guid TEXT, syncStatus INTEGER NOT NULL DEFAULT 0, syncChangeCounter INTEGER NOT NULL DEFAULT 1)
-table|moz_bookmarks_deleted|moz_bookmarks_deleted|9|CREATE TABLE moz_bookmarks_deleted (  guid TEXT PRIMARY KEY, dateRemoved INTEGER NOT NULL DEFAULT 0)
-table|moz_keywords|moz_keywords|11|CREATE TABLE moz_keywords (  id INTEGER PRIMARY KEY AUTOINCREMENT, keyword TEXT UNIQUE, place_id INTEGER, post_data TEXT)
-
-table|moz_anno_attributes|moz_anno_attributes|14|CREATE TABLE moz_anno_attributes (  id INTEGER PRIMARY KEY, name VARCHAR(32) UNIQUE NOT NULL)
-table|moz_annos|moz_annos|16|CREATE TABLE moz_annos (  id INTEGER PRIMARY KEY, place_id INTEGER NOT NULL, anno_attribute_id INTEGER, content LONGVARCHAR, flags INTEGER DEFAULT 0, expiration INTEGER DEFAULT 0, type INTEGER DEFAULT 0, dateAdded INTEGER DEFAULT 0, lastModified INTEGER DEFAULT 0)
-table|moz_items_annos|moz_items_annos|17|CREATE TABLE moz_items_annos (  id INTEGER PRIMARY KEY, item_id INTEGER NOT NULL, anno_attribute_id INTEGER, content LONGVARCHAR, flags INTEGER DEFAULT 0, expiration INTEGER DEFAULT 0, type INTEGER DEFAULT 0, dateAdded INTEGER DEFAULT 0, lastModified INTEGER DEFAULT 0)
-
-table|moz_meta|moz_meta|19|CREATE TABLE moz_meta (key TEXT PRIMARY KEY, value NOT NULL) WITHOUT ROWID
-table|moz_origins|moz_origins|20|CREATE TABLE moz_origins (
-    id INTEGER PRIMARY KEY,
-    prefix TEXT NOT NULL,
-    host TEXT NOT NULL,
-    frecency INTEGER NOT NULL,
-    UNIQUE (prefix, host)
-)
-
-
-    
-    */
     Ok(())
 }
